98adc6098e61fd136e4b22cc4caa1ee13334b762
Osced <osced14@gmail.com>
2017-02-13 19:51:23 -0500

    OnStart
    Primera Versión

---
+++ RPG_Master/Assets/RecorrridoAutonomo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecorrridoAutonomo : MonoBehaviour {
+
+	private NavMeshAgent agente;
+	public Transform destiny;
+
+	// Use this for initialization
+	void Start () {
+	
+		agente = GetComponent <NavMeshAgent>();
+		agente.SetDestination (destiny.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
