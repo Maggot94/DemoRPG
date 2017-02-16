5bfe60d7319ef25e0d2e2383b2322f999ebe4df1
Osced <osced14@gmail.com>
2017-02-16 09:57:08 -0500

    Primera versión ATTACK
    Definición de variables y comportamiento del disparo

---
+++ RPG_Master/Assets/Destriur.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destriur : MonoBehaviour {
+
+	public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter(Collision colision){
+
+		Instantiate (explosion, gameObject.transform.position, gameObject.transform.rotation);
+		Destroy (gameObject);
+		
+	}
+}
---
+++ RPG_Master/Assets/Disparar.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Disparar : MonoBehaviour {
+
+	public GameObject bullet;
+	private GameObject cloneBullet;
+	public float speed=10f;
+
+	// Use this for initialization
+	void Start () {
+	
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Input.GetButtonDown ("Fire1")) {
+
+			cloneBullet = Instantiate (bullet, gameObject.transform.position, bullet.transform.rotation) as GameObject;
+
+			Rigidbody rigBullet = cloneBullet.GetComponent<Rigidbody>();
+
+			rigBullet.AddForce (cloneBullet.transform.forward*speed, ForceMode.Impulse);
+		
+		}
+	
+	}
+
+
+}
